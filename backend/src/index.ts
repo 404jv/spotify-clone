ee423917f2b9839a7f13eb6e1fc2d11506fe12ee
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-03-16 17:58:17 -0300

    Set port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,9 @@
+import variables from "./config/variables";
 import server from "./server";
 import { logger } from "./utils/logger";
 
 server.listen(3000)
-  .on('listening', () => logger.info('🚀 Server is running at: http://localhost:3000'));
+  .on(
+    'listening', 
+    () => logger.info(`🚀 Server is running at: http://localhost:${variables.port}`)
+  );
