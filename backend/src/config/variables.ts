fbeb4c4eb05bc9ff07cfb16a64098ec8037b8e12
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-03-16 17:58:04 -0300

    Create variables

---
+++ backend/src/config/variables.ts
@@ -0,0 +1,11 @@
+
+export default {
+  port: process.env.PORT || 3000,
+  pages: {
+    homeHTML: 'home/index.html',
+    controllerHTML: 'controller/index.html',
+  },
+  location: {
+    home: '/home'
+  }
+}
